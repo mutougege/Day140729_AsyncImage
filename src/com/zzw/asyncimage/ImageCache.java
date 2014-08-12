d4e7242c0c2bca49140bf801e5e4a44627e391ae
mutougege <joyzzw@yeah.net>
2014-08-12 17:46:26 +0800

    lru

---
+++ src/com/zzw/asyncimage/ImageCache.java
@@ -0,0 +1,111 @@
+package com.zzw.asyncimage;
+
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.v4.util.LruCache;
+import android.text.TextUtils;
+import android.util.Log;
+
+/**
+ * Cache-related fields and methods.
+ * 
+ * We use a hard and a soft cache. A soft reference cache is too aggressively cleared by the
+ * Garbage Collector.
+ * 
+ *
+ */
+public class ImageCache {
+
+    public static Bitmap sDefaultIcon;
+    public static Bitmap sDefaultIconBig;
+    public static Bitmap sDefaultBanner;
+    public static Bitmap sDefaultPortrait;
+    
+    private static ImageCache imageCacheInstance;
+    
+    private static LruCache<String, Bitmap> lruCache;
+    
+    public static ImageCache getInstance(Context context) {
+		synchronized (ImageCache.class) {
+			if(imageCacheInstance == null) {
+				imageCacheInstance = new ImageCache(context);
+			}
+		} 
+		
+		return imageCacheInstance;
+	}
+    
+    private ImageCache(Context context) {
+    	BitmapFactory.Options options = new BitmapFactory.Options();
+		options.inPreferredConfig = Bitmap.Config.RGB_565;
+		options.inPurgeable = true;
+		options.inInputShareable = true;
+    	
+    	if (lruCache == null) {
+			lruCache = getBitmapCache(context);
+		}
+	}
+    
+	public synchronized LruCache<String, Bitmap> getBitmapCache(Context context) {
+		if (lruCache == null)
+			buildCache(context);
+		return lruCache;
+	}
+	
+	private void buildCache(Context context) {
+        int memClass = ((ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE)).getMemoryClass();
+
+        int cacheSize = Math.min(Math.max(4, memClass / 15), 8);
+
+        lruCache = new LruCache<String, Bitmap>(1024 * 1024 * cacheSize) {
+            @Override
+            protected int sizeOf(String key, Bitmap bitmap) {
+
+                return bitmap.getRowBytes() * bitmap.getHeight();//bitmap.getByteCount();
+            }
+        };
+    }
+	
+
+    public void addBitmapToCache(String url, Bitmap bitmap) {
+		putToCache(url, bitmap);
+	}
+    
+    private void putToCache(String url, Bitmap bitmap) {
+    	if (url == null || bitmap == null) {
+			return;
+		}
+    	synchronized (lruCache) {
+			lruCache.put(url, bitmap);
+		}
+    }
+    
+    private Bitmap getFromCache(String url) {
+    	if (!TextUtils.isEmpty(url)) {
+	    	synchronized (lruCache) {
+				return lruCache.get(url);
+			}
+    	}
+		return null;
+    }
+    
+	public Bitmap getBitmapFromCache(String url) {
+		
+		return getFromCache(url);
+    }
+	
+    /**
+     * Clears the image cache used internally to improve performance. Note that for memory
+     * efficiency reasons, the cache will automatically be cleared after a certain inactivity delay.
+     */
+	private void clearCache() {
+		lruCache.evictAll();
+	}
+    
+    public void purgeNow() {
+    	clearCache();
+    }
+}
---
+++ src/com/zzw/asyncimage/ImageLoader.java
@@ -0,0 +1,141 @@
+package com.zzw.asyncimage;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Environment;
+import android.util.Log;
+import android.widget.ImageView;
+
+
+public class ImageLoader {
+	private static String TAG = ImageLoader.class.getSimpleName();
+	private File SDcache;
+	private ImageCache memoryCache;
+	private Context context;
+	
+	public ImageLoader(Context context) {
+		this.context = context;
+		memoryCache = ImageCache.getInstance(context);
+	}
+
+	public void download(String url, ImageView imageView, int imageType) {
+		if (!isAllowImage()) {
+			return;
+		}
+
+		Bitmap bitmap = memoryCache.getBitmapFromCache(url);
+		if (bitmap == null) {
+			Log.i(TAG, "bitmap == null");
+			asyncloadImage(url, imageView, imageType);
+		} else {
+			Log.i(TAG, "bitmap != null");
+			imageView.setImageBitmap(bitmap);
+		}
+	}
+	
+	public boolean isAllowImage() {
+		return true;
+	}
+	
+	public void asyncloadImage(String path, ImageView iv_header, int imageType) {  
+        AsyncImageTask task = new AsyncImageTask(iv_header);  
+        task.execute(path);  
+    }  
+  
+    private final class AsyncImageTask extends AsyncTask<String, Integer, Bitmap> {
+  
+        private ImageView iv_header;  
+  
+        public AsyncImageTask(ImageView iv_header) {  
+            this.iv_header = iv_header;  
+        }  
+  
+        /** 
+         * 将在onPreExecute 方法执行后马上执行，该方法运行在后台线程中。 
+         * 这里将主要负责执行那些很耗时的后台计算工作。可以调用  
+         * publishProgress方法来更新实时的任务进度。该方法是抽象方法， 
+         * 子类必须实现。  
+         */ 
+        @Override  
+        protected Bitmap doInBackground(String... params) {  
+            try {  
+                return getImageURI(params[0]);  
+            } catch (Exception e) {  
+                e.printStackTrace();  
+            }  
+            return null;  
+        }
+  
+        // 这个放在ui线程中执行  
+        @Override  
+        protected void onPostExecute(Bitmap bitmap) {  
+            super.onPostExecute(bitmap);   
+            // 完成图片的绑定  
+            if (iv_header != null && bitmap != null) {  
+                iv_header.setImageBitmap(bitmap);
+            }  
+        }  
+    }
+    
+    /*
+	 * 从网络上获取图片，如果图片在本地存在的话就直接拿，如果不存在再去服务器上下载图片 这里的path是图片的地址
+	 */
+	public Bitmap getImageURI(String path) throws Exception {
+		
+		Bitmap bitemap = null;
+		//String name = MD5.getMD5(path) + path.substring(path.lastIndexOf("."));
+		String name = MD5.getMD5(path) +".bin";
+		
+		// 创建缓存目录，系统一运行就得创建缓存目录的，
+		SDcache = new File(Environment.getExternalStorageDirectory(), "cache");
+		if (!SDcache.exists()) {
+			SDcache.mkdirs();
+		}
+		
+		File file = new File(SDcache, name);
+		// 如果图片存在本地缓存目录，则不去服务器下载
+		if (file.exists()&&file.length()>0) {
+			
+			FileInputStream is = new FileInputStream(file);
+			bitemap = BitmapFactory.decodeStream(is);
+			memoryCache.addBitmapToCache(path, bitemap);
+			is.close();
+			return bitemap;
+		} else {
+			// 从网络上获取图片
+			URL url = new URL(path);
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setConnectTimeout(5000);
+			conn.setRequestMethod("GET");
+			conn.setDoInput(true);
+			if (conn.getResponseCode() == 200) {
+
+				InputStream is = conn.getInputStream();
+				FileOutputStream fos = new FileOutputStream(file);
+				byte[] buffer = new byte[4*1024];
+				int len = 0;
+				while ((len = is.read(buffer)) != -1) {
+					fos.write(buffer, 0, len);
+				}
+				bitemap = BitmapFactory.decodeStream(is);
+				memoryCache.addBitmapToCache(path, bitemap);
+				is.close();
+				fos.close();
+				// 返回一个URI对象
+				return bitemap;
+			}
+		}
+		return null;
+	}
+}
